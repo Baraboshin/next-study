6c5dc1d15ca2136555e9ce613ce96b84e2fe61e3
Антон Барабошин <baraboshin@neolant.com>
2021-07-02 19:33:43 +0300

    NT-1 - 1)Added concurrently to project (its for to start two or more scripts from package.json). 2)Added fetch posts.

--- pages/posts.js
+++ pages/posts.js
@@ -1,9 +1,26 @@
 import {MainLayout} from "../layouts/mainlayout";
+import  { useState, useEffect } from 'react'
 
 export default function About() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    async function load() {
+      const response = await fetch('http://localhost:4200/posts')
+      const json = await response.json()
+      setPosts(json)
+      console.log(json)
+    }
+    load()
+  }, [])
+
   return(
     <MainLayout title={'Posts Page'}>
       <h1>Posts</h1>
+      <ul>
+        <li></li>
+      </ul>
+      <pre>{JSON.stringify(posts)}</pre>
     </MainLayout>
   )
 }
