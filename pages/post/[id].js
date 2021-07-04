a7902e4dc36699c6741176a36abf0a9444c45e63
Антон Барабошин <baraboshin@neolant.com>
2021-07-04 16:16:00 +0300

    NT-1 - getServerSideProps(Отрабатывает только на сервере)

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -37,12 +37,21 @@ export default function Post({ post: serverPost }) {
   )
 }
 
-Post.getInitialProps = async({ query, req }) => {
-  if (!req) return { post: null }
+// Post.getInitialProps = async({ query, req }) => {
+//   if (!req) return { post: null }
+//   const response = await fetch(`http://localhost:4200/posts/${query.id}`)
+//   const post = await response.json()
+//   return {
+//     post,
+//   }
+// }
+
+export async function getServerSideProps({ query, req }) {
   const response = await fetch(`http://localhost:4200/posts/${query.id}`)
   const post = await response.json()
   return {
-    post,
+    props: {
+      post,
+    }, // will be passed to the page component as props
   }
 }
-
