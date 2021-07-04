7402a7f9fb88b3a821ec25ac8a8f57273e87e4a1
Антон Барабошин <baraboshin@neolant.com>
2021-07-04 21:49:54 +0300

    NT-1 - Loading-indicator (nextjs-progressbar lid) + Загрузка данных на странице About.

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,18 @@
 import '../styles/globals.scss'
+import NextNprogress from 'nextjs-progressbar';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNprogress
+        color="yellow"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
+      <Component {...pageProps} />
+    </>
+    )
 }
